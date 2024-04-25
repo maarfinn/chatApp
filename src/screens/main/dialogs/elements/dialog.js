fb252a8cbe406545657223a3f3e16903e311fd9e
maarfinn <asfahan.arfinn@gmail.com>
2024-04-25 17:33:24 +0530

    changes

--- src/screens/main/dialogs/elements/dialog.js
+++ src/screens/main/dialogs/elements/dialog.js
@@ -1,35 +1,28 @@
-import React from 'react'
-import { StyleSheet, View, TouchableOpacity } from 'react-native'
-import Avatar from '../../../components/avatar'
-import DialogTitle from './dialogTitle'
-import DialogLastDate from './dialogLastDate'
-import DialogUnreadCounter from './dialogUnreadCounter'
-import UsersService from '../../../../services/users-service'
-import { DIALOG_TYPE } from '../../../../helpers/constants'
+import React from "react";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import Avatar from "../../../components/avatar";
+import DialogTitle from "./dialogTitle";
+import DialogLastDate from "./dialogLastDate";
+import DialogUnreadCounter from "./dialogUnreadCounter";
+import UsersService from "../../../../services/users-service";
+import { DIALOG_TYPE } from "../../../../helpers/constants";
 
-export default function Dialog ({dialog, onDialogClick}) {
-
+export default function Dialog({ dialog, onDialogClick }) {
   const onPress = async () => {
-    onDialogClick(dialog)
-  }
+    onDialogClick(dialog);
+  };
 
-  const dialogPhoto = dialog.type === DIALOG_TYPE.PRIVATE 
-      ? UsersService.getUsersAvatar(dialog.occupants_ids)
+  const dialogPhoto =
+    dialog.type === DIALOG_TYPE.PRIVATE
+      ? UsersService?.getUsersAvatar(dialog.occupants_ids)
       : dialog.photo;
 
   return (
     <TouchableOpacity onPress={onPress}>
       <View style={styles.container}>
-        <Avatar
-          photo={dialogPhoto}
-          name={dialog.name}
-          iconSize="large"
-        />
-        <View style={styles.border} >
-          <DialogTitle
-            name={dialog.name}
-            message={dialog.last_message}
-          />
+        <Avatar photo={dialogPhoto} name={dialog.name} iconSize="large" />
+        <View style={styles.border}>
+          <DialogTitle name={dialog.name} message={dialog.last_message} />
           <View style={styles.infoContainer}>
             <DialogLastDate
               lastDate={dialog.last_message_date_sent}
@@ -42,31 +35,31 @@ export default function Dialog ({dialog, onDialogClick}) {
           </View>
         </View>
       </View>
-    </TouchableOpacity >
-  ) 
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    paddingHorizontal: 10
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    paddingHorizontal: 10,
   },
   border: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     borderBottomWidth: 0.5,
-    borderBottomColor: 'lightgrey'
+    borderBottomColor: "lightgrey",
   },
   infoContainer: {
     maxWidth: 75,
     height: 50,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end',
+    justifyContent: "flex-start",
+    alignItems: "flex-end",
     paddingVertical: 10,
-    marginLeft: 5
-  }
-})
+    marginLeft: 5,
+  },
+});
